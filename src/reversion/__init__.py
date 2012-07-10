0762c686dc559d9f2fe9984fc5b0b62430045b79
andrewshkovskii <andrewshkovskiI@gmail.com>
2012-07-10 16:29:58 +0400

    added reverted signal import

--- src/reversion/__init__.py
+++ src/reversion/__init__.py
@@ -10,7 +10,7 @@ import django, warnings
 
 from reversion.revisions import default_revision_manager, revision_context_manager, VersionAdapter
 from reversion.admin import VersionAdmin
-from reversion.models import pre_revision_commit, post_revision_commit
+from reversion.models import pre_revision_commit, post_revision_commit, reverted
 from reversion.version import __version__ 
 from reversion.views import RevisionsListView, RevisionRevertFormView, ReversionDeleteMixin
 
